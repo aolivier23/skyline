6dce89b454a772c02b9f2534db7b8c0f30cf99ce
Andrew Olivier <andrew@djoinc.com>
2019-11-09 09:45:38 -0600

    Updated groundPlane.cpp with intersection calculation.

--- serial/groundPlane.cpp
+++ serial/groundPlane.cpp
@@ -7,19 +7,20 @@
 #endif //ON_DEVICE
 
 //Distance from a ray's origin to its intersection with y = 0.
-CL(float) plane_intersect(const ray thisRay)
+CL(float) groundPlane_intersect(const ray thisRay)
 {
+  return -ray.position.y/ray.direction.y;
 }
 
 //Normal at a point on a plane
-CL(float3) plane_normal(const CL(float3) /*pos*/)
+CL(float3) groundPlane_normal(const CL(float3) /*pos*/)
 {
   return (CL(float3)){0., 0., 1.};
 }
 
 //Coordinates into a rectangular texture on this plane.
 //Normalized so that the original texture fits into a space of size texNorm.
-CL(float2) plane_tex_coords(const CL(float2) texNorm, const CL(float3) pos)
+CL(float2) groundPlane_tex_coords(const CL(float2) texNorm, const CL(float3) pos)
 {
   return (CL(float2)){pos.x / texNorm.x, pos.z / texNorm.y};
 }
